0ec29372e58bf50f26fcbb17c0e8c8390b9dfe0d
Andrew-webdev <andreyrow96@gmail.com>
2020-03-29 20:11:21 +0300

    feat: made layout

---
+++ script.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-undef */
+/* eslint-disable eol-last */
+
+
+// const container = document.createElement('div');
+// container.className = 'container';
+// document.body.prepend(container);
+
+// const textarea = document.createElement('textarea');
+// textarea.className = 'textarea';
+// container.prepend(textarea);
+
+// const keyboard = document.createElement('div');
+// keyboard.className = 'keyboard';
+// container.append(keyboard);
+
+// const keyboardKeys = document.createElement('div');
+// keyboardKeys.className = 'keyboard__keys';
+// keyboard.append(keyboardKeys);
+
+// const keyboardKey = document.createElement('button');
+// keyboardKey.className = 'keyboard__key';
+// keyboardKey.innerHTML = 'a';
+// keyboardKeys.append(keyboardKey);
