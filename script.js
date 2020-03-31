0f2a0a4f0dbe47146a36732b59e9bd9ad678a62b
Andrew-webdev <andreyrow96@gmail.com>
2020-03-31 20:41:37 +0300

    feat: add js layout and click on real keyboard

--- script.js
+++ script.js
@@ -2,23 +2,53 @@
 /* eslint-disable eol-last */
 
 
-// const container = document.createElement('div');
-// container.className = 'container';
-// document.body.prepend(container);
+// Create layout
 
-// const textarea = document.createElement('textarea');
-// textarea.className = 'textarea';
-// container.prepend(textarea);
+const container = document.createElement('div');
+container.className = 'container';
+document.body.prepend(container);
 
-// const keyboard = document.createElement('div');
-// keyboard.className = 'keyboard';
-// container.append(keyboard);
+const textarea = document.createElement('textarea');
+textarea.className = 'textarea';
+container.prepend(textarea);
 
-// const keyboardKeys = document.createElement('div');
-// keyboardKeys.className = 'keyboard__keys';
-// keyboard.append(keyboardKeys);
+const keyboard = document.createElement('div');
+keyboard.className = 'keyboard';
+container.append(keyboard);
 
-// const keyboardKey = document.createElement('button');
-// keyboardKey.className = 'keyboard__key';
-// keyboardKey.innerHTML = 'a';
-// keyboardKeys.append(keyboardKey);
+const keyboardKeys = document.createElement('div');
+keyboardKeys.className = 'keyboard__keys';
+keyboard.append(keyboardKeys);
+
+
+// Get key on keyboard
+
+const key = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101,
+  114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106,
+  107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47,
+];
+
+// Click on real keyboard
+
+function init() {
+  let out = '';
+  for (let i = 0; i < key.length; i += 1) {
+    if (i === 13 || i === 26 || i === 37) {
+      out += '<br>';
+    }
+    out += `<button class="keyboard__key" data = "${key[i]}" >${String.fromCharCode(key[i])}</button>`;
+  }
+  document.querySelector('.keyboard__keys').innerHTML = out;
+}
+init();
+
+document.onkeypress = function (event) {
+  document.querySelector('.textarea').innerHTML += event.key;
+  console.log(event.code);
+  console.log(event.keyCode);
+  document.querySelectorAll('.keyboard__keys .keyboard__key').forEach((elem) => {
+    elem.classList.remove('active');
+  });
+
+  document.querySelector(`.keyboard__keys .keyboard__key[data = "${event.keyCode}"]`).classList.add('active');
+};
